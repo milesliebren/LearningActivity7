f97ccac057a6180ff3735e7a3b19bf065e6fef7d
Miles Liebren <mliebren@css.edu>
2024-10-17 11:22:41 -0500

    Add project files.

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using OrderService.Models;
+using ProductService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private static List<Order> orders = new List<Order>
+    {
+        new Order
+        {
+            OrderId = 1,
+            OrderDate = DateTime.Now,
+            CustomerName = "John Doe",
+            Products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product A", Price = 99.99m },
+                new Product { Id = 2, Name = "Product B", Price = 49.99m }
+            }
+        }
+    };
+
+        [HttpGet]
+        public IEnumerable<Order> GetOrders()
+        {
+            return orders;
+        }
+    }
+
+}
---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using ProductService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningActivity7.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Product A", Price = 99.99m },
+        new Product { Id = 2, Name = "Product B", Price = 49.99m },
+        new Product { Id = 3, Name = "Product C", Price = 149.99m }
+    };
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return products;
+        }
+    }
+}
